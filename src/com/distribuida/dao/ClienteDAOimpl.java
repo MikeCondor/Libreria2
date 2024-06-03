65f0d6def6ec4332cd6b878288297929a0bfaf5b
kherrera <kherrera@tecnologicoismac.edu.ec>
2024-06-03 11:02:13 -0500

    MCv003

---
+++ src/com/distribuida/dao/ClienteDAOimpl.java
@@ -0,0 +1,39 @@
+package com.distribuida.dao;
+
+import java.util.List;
+
+import com.distribuida.entities.Cliente;
+
+public class ClienteDAOimpl implements ClienteDAO {
+
+	@Override
+	public List<Cliente> findALL() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cliente findOne(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(Cliente cliente) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void up(Cliente cliente) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dell(Cliente cliente) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
