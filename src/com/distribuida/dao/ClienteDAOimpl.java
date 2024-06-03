d6ff75940cce5a8cb10a5ac3693f479c0eea7b45
MATUTINO <MATUTINO@CuartoSoft>
2024-06-03 11:50:31 -0500

    DMv0004 manejo de patron DAO

--- src/com/distribuida/dao/ClienteDAOimpl.java
+++ src/com/distribuida/dao/ClienteDAOimpl.java
@@ -2,14 +2,28 @@ package com.distribuida.dao;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.distribuida.entities.Cliente;
 
+@Repository
 public class ClienteDAOimpl implements ClienteDAO {
+	
+	@Autowired
+	private SessionFactory sessionFactory;
 
 	@Override
+	@Transactional
 	public List<Cliente> findALL() {
 		// TODO Auto-generated method stub
-		return null;
+		
+		Session session = sessionFactory.getCurrentSession();
+		return session.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 
 	@Override
